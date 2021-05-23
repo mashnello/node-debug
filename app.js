710531aaf5530afb1e256026c45272c5a5f6518c
mashnello <>
2021-05-23 21:33:00 +0200

    refactor: add const for port

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const db = require('./db');
 const user = require('./controllers/usercontroller');
 const game = require('./controllers/gamecontroller')
 
+const PORT = 4000
 
 db.sync();
 app.use(require('body-parser').json());
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(4000, function() {
+app.listen(PORT, function() {
     console.log("App is listening on 4000");
 })
