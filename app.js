0ff5e9ecd6ef59eb03c4007d72e524b9e81aafda
mashnello <>
2021-05-23 21:51:43 +0200

    refactor: remove body-parser

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const game = require('./controllers/gamecontroller')
 const PORT = 4000
 
 db.sync().then(() => {
-    app.use(require('body-parser').json());
+    app.use(express.json());
     app.use('/api/auth', user);
     app.use(require('./middleware/validate-session'))
     app.use('/api/game', game);
